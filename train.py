c84e70f0d075801d89d24d6ddc4d7411fac5d2cc
Alexander Kirko <alexander.kirko@gmail.com>
2019-03-16 09:45:36 +0300

    feat: add imports to train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+from torch import optim
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+
+import json
+import time
+
+import math
+import numpy as np
+
